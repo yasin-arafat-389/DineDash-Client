ecb5982543d2d44b8025cd56e974d4b91f96797f
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-04-26 20:11:13 +0600

    Redesigned the animations for hero section

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useMemo, useRef } from "react";
 import BrowseFoods from "../../../Utility/FancyButton/BrowseFoods/BrowseFoods";
 import "./HeroSection.css";
+import { useInView } from "framer-motion";
 
 const HeroSection = () => {
   const images = useMemo(
@@ -14,37 +15,14 @@ const HeroSection = () => {
     []
   );
 
-  const divRef = useRef(null);
-  const [isOnScreen, setIsOnScreen] = useState(false);
+  // Animation refs
+  const titleRef = useRef(null);
+  const imagesRef = useRef(null);
+  const heroSectionRef = useRef(null);
 
-  const handleIntersection = (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        setIsOnScreen(true);
-      } else {
-        setIsOnScreen(false);
-      }
-    });
-  };
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleIntersection, {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.5,
-    });
-
-    if (divRef.current) {
-      observer.observe(divRef.current);
-    }
-
-    const currentDivRef = divRef.current;
-    return () => {
-      if (currentDivRef) {
-        observer.unobserve(currentDivRef);
-      }
-    };
-  }, []);
+  const isInView = useInView(titleRef, { once: false });
+  const isImageInView = useInView(imagesRef, { once: false });
+  const isHeroSectionInView = useInView(heroSectionRef, { once: false });
 
   return (
     <div>
@@ -57,38 +35,64 @@ const HeroSection = () => {
         }}
       >
         <div className="w-[85%] mx-auto pt-10">
-          <section className="mb-8 flex flex-col md:flex-row lg:flex-row justify-center gap-10 ">
+          <section
+            className="mb-8 flex flex-col md:flex-row lg:flex-row justify-center gap-10 "
+            ref={heroSectionRef}
+          >
             <div className="flex flex-col justify-center sm:text-center lg:py-12 lg:text-left w-5/5 md:w-3/5 lg:w-3/5">
-              <p className="mb-4 font-semibold text-indigo-500 md:mb-6 md:text-lg xl:text-xl text-center md:text-center lg:text-left">
+              <p
+                className={`mb-4 font-semibold text-indigo-500 md:mb-6 md:text-lg xl:text-xl text-center md:text-center lg:text-left ${
+                  isHeroSectionInView
+                    ? `translate-y-0 duration-[0.6s] opacity-100 ease-in-out`
+                    : `translate-y-[50px] opacity-0 ease-in-out`
+                }`}
+              >
                 Taste the Magic at Your Doorstep
               </p>
 
-              <h1 className="mb-5 md:mb-5 lg:mb-12 text-[30px] md:text-[30px] lg:text-[50px] leading-[30px] md:leading-[40px] lg:leading-[60px] font-bold text-black text-center md:text-center lg:text-left">
+              <h1
+                className={`mb-5 md:mb-5 lg:mb-12 text-[30px] md:text-[30px] lg:text-[50px] leading-[30px] md:leading-[40px] lg:leading-[60px] font-bold text-black text-center md:text-center lg:text-left ${
+                  isHeroSectionInView
+                    ? `translate-y-0 duration-[0.6s] opacity-100 delay-[0.3s] ease-in-out`
+                    : `translate-y-[50px] opacity-0 ease-in-out`
+                }`}
+              >
                 Experience the Next Generation of Food Delivery Magic
               </h1>
 
-              <div className="flex flex-col gap-2.5 sm:flex-row items-center md:items-start lg:items-start my-5 md:my-0 lg:my-5">
+              <div
+                className={`flex flex-col gap-2.5 sm:flex-row items-center md:items-start lg:items-start my-5 md:my-0 lg:my-5  ${
+                  isHeroSectionInView
+                    ? `translate-y-0 duration-[0.6s] opacity-100 delay-[0.5s] ease-in-out`
+                    : `translate-y-[50px] opacity-0 ease-in-out`
+                }`}
+              >
                 <BrowseFoods />
               </div>
             </div>
 
-            <div className="w-5/5 md:w-2/5 lg:w-2/5">
+            <div className="w-5/5 md:w-2/5 lg:w-2/5 overflow-hidden">
               <img
                 src="https://i.ibb.co/jwbhTXt/7509757-3683230-removebg-preview-1.png"
                 loading="lazy"
                 alt="Photo by Fakurian Design"
-                className="h-full w-full object-cover object-center"
+                className={`h-full w-full object-cover object-center ${
+                  isHeroSectionInView
+                    ? `translate-x-0 duration-[0.8s] ease-in-out`
+                    : `translate-x-[550px]`
+                }`}
               />
             </div>
           </section>
 
           <div className="mt-20 text-center">
-            <div ref={divRef} className="w-[75%] mx-auto overflow-hidden">
+            <div className="w-[75%] mx-auto overflow-hidden">
               <h2
+                ref={titleRef}
                 className={`text-[20px] md:text-[28px] lg:text-[40px] font-bold leading-tight text-gray-800 mb-[30px] md:mb-[50px] lg:mb-[70px] ${
-                  isOnScreen
-                    ? "ease-in-out duration-1000 translate-x-0"
-                    : "translate-x-[-700px]"
+                  isInView
+                    ? "translate-x-0 duration-1000 ease-in-out"
+                    : "translate-x-[-200px] md:translate-x-[-800px]"
                 }`}
               >
                 Trusted by <span className="text-blue-600">Restaurants</span>{" "}
@@ -96,13 +100,23 @@ const HeroSection = () => {
               </h2>
             </div>
 
-            <section className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-10 justify-items-center">
+            <section
+              ref={imagesRef}
+              className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-10 justify-items-center"
+            >
               {images.map((src, index) => (
                 <img
                   key={index}
                   src={src}
                   alt={`Image ${index}`}
-                  className="w-[100px] md:w-[60%] lg:w-[60%]"
+                  className={`w-[100px] md:w-[60%] lg:w-[60%] ${
+                    isImageInView
+                      ? `translate-y-0 duration-1000 ease-in-out opacity-100`
+                      : `translate-y-[-100px] opacity-0`
+                  }`}
+                  style={{
+                    transitionDelay: isImageInView ? `${index * 0.5}s` : "0s",
+                  }}
                 />
               ))}
             </section>
