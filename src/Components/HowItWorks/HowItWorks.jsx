bb1c7426328409a7e1519671b4e4b294a108b67a
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-05-03 16:53:54 +0600

    Redesigned scroll animation to how it works section

--- src/Components/HowItWorks/HowItWorks.jsx
+++ src/Components/HowItWorks/HowItWorks.jsx
@@ -1,17 +1,29 @@
-import { useInView } from "framer-motion";
-import { useRef } from "react";
+import { motion } from "framer-motion";
 
 const HowItWorks = () => {
-  // animation refs
-  const stepImageRef = useRef(null);
-  const titleRef = useRef(null);
-  const descriptionRef = useRef(null);
-
-  const isStepImageInView = useInView(stepImageRef, { once: false });
-  const isStepTitleInView = useInView(titleRef, { once: false });
-  const isStepDescriptionInView = useInView(descriptionRef, {
-    once: false,
-  });
+  let steps = [
+    {
+      thumbnail: "./step-1.png",
+      step: "01",
+      title: "Select Restaurant",
+      description:
+        "Begin your culinary journey by selecting from a diverse array of top-rated restaurants on DineDash, each offering a unique and tantalizing dining experience.",
+    },
+    {
+      thumbnail: "./step-2.png",
+      step: "02",
+      title: "Select Food",
+      description:
+        "Indulge in a seamless selection process as you pick from our extensive menu, featuring a tantalizing array of dishes crafted to satisfy every craving and preference.",
+    },
+    {
+      thumbnail: "./step-3.png",
+      step: "03",
+      title: "Wait for delivery",
+      description:
+        "Savor the anticipation as our dedicated delivery team swiftly transports your chosen delicacies, ensuring they arrive fresh and delectable at your doorstep, ready for your enjoyment.",
+    },
+  ];
 
   return (
     <div className="mt-10 md:mt-10 lg:mt-20 mb-10">
@@ -32,122 +44,40 @@ const HowItWorks = () => {
       </div>
 
       <div className="images&description grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-10 w-[90%] mx-auto mt-8">
-        {/* Card 1 */}
-        <div className="flex flex-col items-center">
-          <img
-            ref={stepImageRef}
-            src="./step-1.png"
-            alt=""
-            className={`${
-              isStepImageInView
-                ? `scale-100 opacity-100 duration-[1.3s] ease-in-out`
-                : `scale-0 opacity-0`
-            }`}
-          />
-          <div className="flex items-center my-2">
-            <h1
-              ref={titleRef}
-              className={`text-[24px] text-[#363636] font-bold ${
-                isStepTitleInView
-                  ? `translate-y-0 opacity-100 duration-[0.8s] delay-[0.3s] ease-in-out`
-                  : `translate-y-[100px] opacity-0`
-              }`}
-            >
-              <span className={`text-[40px] text-[#cfcfcf] font-bold`}>01</span>{" "}
-              Select Restaurant
-            </h1>
-          </div>
-          <p
-            ref={descriptionRef}
-            className={`text-[16px] text-[#787878] text-center font-medium ${
-              isStepDescriptionInView
-                ? `translate-y-0 opacity-100 duration-[0.8s] delay-[0.5s] ease-in-out`
-                : `translate-y-[100px] opacity-0`
-            }`}
-          >
-            Begin your culinary journey by selecting from a diverse array of
-            top-rated restaurants on DineDash, each offering a unique and
-            tantalizing dining experience.
-          </p>
-        </div>
+        {steps.map((step, index) => (
+          <div className="flex flex-col items-center" key={index}>
+            <motion.img
+              initial={{ scale: 0 }}
+              whileInView={{ scale: 1 }}
+              transition={{ duration: 0.8 }}
+              src={step.thumbnail}
+              alt=""
+            />
 
-        {/* Card 2 */}
-        <div className="flex flex-col items-center">
-          <img
-            ref={stepImageRef}
-            src="./step-2.png"
-            alt=""
-            className={`${
-              isStepImageInView
-                ? `scale-100 opacity-100 duration-[1.3s] ease-in-out`
-                : `scale-0 opacity-0`
-            }`}
-          />
-          <div className="flex items-center my-2">
-            <h1
-              ref={titleRef}
-              className={`text-[24px] text-[#363636] font-bold ${
-                isStepTitleInView
-                  ? `translate-y-0 opacity-100 duration-[0.8s] delay-[0.3s] ease-in-out`
-                  : `translate-y-[100px] opacity-0`
-              }`}
+            <motion.div
+              initial={{ y: 100, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.8, delay: 0.2 }}
+              className="flex items-center my-2"
             >
-              <span className="text-[40px] text-[#cfcfcf] font-bold">02</span>{" "}
-              Select Food
-            </h1>
-          </div>
-          <p
-            ref={descriptionRef}
-            className={`text-[16px] text-[#787878] text-center font-medium ${
-              isStepDescriptionInView
-                ? `translate-y-0 opacity-100 duration-[0.8s] delay-[0.5s] ease-in-out`
-                : `translate-y-[100px] opacity-0`
-            }`}
-          >
-            Indulge in a seamless selection process as you pick from our
-            extensive menu, featuring a tantalizing array of dishes crafted to
-            satisfy every craving and preference.
-          </p>
-        </div>
+              <h1 className={`text-[24px] text-[#363636] font-bold `}>
+                <span className={`text-[40px] text-[#cfcfcf] font-bold`}>
+                  {step.step}
+                </span>{" "}
+                {step.title}
+              </h1>
+            </motion.div>
 
-        {/* Card 3 */}
-        <div className="flex flex-col items-center">
-          <img
-            ref={stepImageRef}
-            src="./step-3.png"
-            alt=""
-            className={`${
-              isStepImageInView
-                ? `scale-100 opacity-100 duration-[1.3s] ease-in-out`
-                : `scale-0 opacity-0`
-            }`}
-          />
-          <div className="flex items-center my-2">
-            <h1
-              ref={titleRef}
-              className={`text-[24px] text-[#363636] font-bold ${
-                isStepTitleInView
-                  ? `translate-y-0 opacity-100 duration-[0.8s] delay-[0.3s] ease-in-out`
-                  : `translate-y-[100px] opacity-0`
-              }`}
+            <motion.p
+              initial={{ y: 100, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.8, delay: 0.4 }}
+              className={`text-[16px] text-[#787878] text-center font-medium `}
             >
-              <span className="text-[40px] text-[#cfcfcf] font-bold">03</span>{" "}
-              Wait for delivery
-            </h1>
+              {step.description}
+            </motion.p>
           </div>
-          <p
-            ref={descriptionRef}
-            className={`text-[16px] text-[#787878] text-center font-medium ${
-              isStepDescriptionInView
-                ? `translate-y-0 opacity-100 duration-[0.8s] delay-[0.5s] ease-in-out`
-                : `translate-y-[100px] opacity-0`
-            }`}
-          >
-            Savor the anticipation as our dedicated delivery team swiftly
-            transports your chosen delicacies, ensuring they arrive fresh and
-            delectable at your doorstep, ready for your enjoyment.
-          </p>
-        </div>
+        ))}
       </div>
     </div>
   );
